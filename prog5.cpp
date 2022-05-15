f9fd9bc778d66f7ed95a8be3929981d4ceca4819
Vansh1498 <101710029+Vansh1498@users.noreply.github.com>
2022-05-16 00:45:09 +0530

    updated program for factorial

--- prog5.cpp
+++ prog5.cpp
@@ -13,3 +13,22 @@ int main() {
     int output = factorial(n);
     cout << output << endl;
 }
+
+//or
+
+int factorial(int n) {
+    if (n==0) {
+        return 1;
+    }
+    int smalloutput = factorial(n-1);
+    int output = n * smalloutput;
+    return output;
+}
+
+int main() { 
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+}             
+
+
