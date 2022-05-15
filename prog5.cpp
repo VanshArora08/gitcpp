9989f37ed329b602af852446c3b4629a737204ba
Vansh1498 <101710029+Vansh1498@users.noreply.github.com>
2022-05-16 00:48:31 +0530

    calc x to the power n

--- prog5.cpp
+++ prog5.cpp
@@ -32,3 +32,24 @@ int main() {
 }             
 
 
+/*x^n*/
+int pow(int x, int n) {
+    if (n == 0) {
+        return 1;
+    }    
+    
+    else if (n % 2 == 0) {
+        return pow(x, n / 2) * pow(x, n / 2);     
+    }
+    
+    else {
+         return x * pow(x, n / 2) * pow(x, n / 2);
+    }
+       
+    
+int main() {
+    int x, n;
+    cin >> x;
+    cin >> n;
+    cout << pow(x, n) << endl;
+}
